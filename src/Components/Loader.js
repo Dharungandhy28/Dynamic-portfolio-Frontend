85a47c0b3644da6f5d50021b655a37b14bd214cb
Dharungandhy28 <dharungandhi28@gmail.com>
2024-05-02 14:39:03 +0530

    Loader fixed

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 function Loader() {
+  const { portfolioData } = useSelector((state) => state.root);
+  const { firstName, lastName, middleName } = portfolioData.intro;
   return (
     <div className="h-screen flex items-center justify-center fixed inset-0 bg-primary z-[10000]">
       <div className="flex gap-5 text-6xl font-semibold">
-        <h1 className="text-secondary M">M</h1>
-        <h1 className="text-white D">D</h1>
-        <h1 className="text-tertiary G">G</h1>
+        <h1 className="text-secondary M">{firstName[0]}</h1>
+        <h1 className="text-white D">{middleName[0]}</h1>
+        <h1 className="text-tertiary G">{lastName[0]}</h1>
       </div>
     </div>
   );
